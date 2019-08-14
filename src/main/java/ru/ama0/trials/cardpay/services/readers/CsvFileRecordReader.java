3dc3e2a40501997d2df2ae1337cb4116c0fc6a51
Alexander Melnichuk <alexander.melnichuk@gmail.com>
2019-08-14 16:30:07 +1000

    Extracted createRawRecord method

--- src/main/java/ru/ama0/trials/cardpay/services/readers/CsvFileRecordReader.java
+++ src/main/java/ru/ama0/trials/cardpay/services/readers/CsvFileRecordReader.java
@@ -19,37 +19,40 @@ public class CsvFileRecordReader extends AbstractFileRecordReader {
 
     @Override
     public Void call() throws Exception {
-        RawRecord rawRecord;
         try (FileReader fileReader = new FileReader(file);
              BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             long lineNumber = 1;
             while (bufferedReader.ready()) {
-                String row = bufferedReader.readLine();
-                String[] fields = row.split(csvSeparator);
-                if (fields.length < 4) {
-                    rawRecord = RawRecord.builder()
-                            .orderId(fields[0])
-                            .amount(fields.length > 1 ? fields[1] : "")
-                            .currency(fields.length > 2 ? fields[2] : "")
-                            .comment("")
-                            .filename(file.getName())
-                            .line(lineNumber)
-                            .result(String.format("Missing %d required field(s)", 4 - fields.length))
-                            .build();
-                } else {
-                    rawRecord = RawRecord.builder()
-                            .orderId(fields[0])
-                            .amount(fields[1])
-                            .currency(fields[2])
-                            .comment(fields[3])
-                            .filename(file.getName())
-                            .line(lineNumber)
-                            .build();
-                }
+                readQueue.put(createRawRecord(lineNumber, bufferedReader.readLine()));
                 lineNumber++;
-                readQueue.put(rawRecord);
             }
         }
         return null;
+    }
+
+    private RawRecord createRawRecord(long lineNumber, String row) {
+        RawRecord rawRecord;
+        String[] fields = row.split(csvSeparator);
+        if (fields.length < 4) {
+            rawRecord = RawRecord.builder()
+                    .orderId(fields[0])
+                    .amount(fields.length > 1 ? fields[1] : "")
+                    .currency(fields.length > 2 ? fields[2] : "")
+                    .comment("")
+                    .filename(file.getName())
+                    .line(lineNumber)
+                    .result(String.format("Missing %d required field(s)", 4 - fields.length))
+                    .build();
+        } else {
+            rawRecord = RawRecord.builder()
+                    .orderId(fields[0])
+                    .amount(fields[1])
+                    .currency(fields[2])
+                    .comment(fields[3])
+                    .filename(file.getName())
+                    .line(lineNumber)
+                    .build();
+        }
+        return rawRecord;
     }
 }
